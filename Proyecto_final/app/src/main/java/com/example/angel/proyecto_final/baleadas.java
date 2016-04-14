ac2823ca14055b67c35693bd8c244b4170966f15
Aleman1414 <angel.aleman1414@gmail.com>
2016-04-14 16:42:55 -0600

    Cambio Prueba

--- Proyecto_final/app/src/main/java/com/example/angel/proyecto_final/baleadas.java
+++ Proyecto_final/app/src/main/java/com/example/angel/proyecto_final/baleadas.java
@@ -18,6 +18,7 @@ public class baleadas extends AppCompatActivity {
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
+            //jbiyvuvyu
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
